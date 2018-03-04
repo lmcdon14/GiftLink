2209452c253f5a7fa3a2742c1f75d7a0d764dffe
Isaac Ige <35306862+iige@users.noreply.github.com>
2018-03-04 17:39:36 -0500

    Add files via upload

--- gift_link.py
+++ gift_link.py
@@ -5,44 +5,15 @@ import time
 import requests
 
 #GiftLink's manual login flow URL
-login_url = \
-"""
-  https://www.facebook.com/v2.12/dialog/oauth?
-  client_id=2050570888522386
-  &redirect_uri=https://www.facebook.com/connect/login_success.html
-  &state="{st=cs340,ds=gift}"
-  &response_type=token
-  &scope=public_profile,user_friends
- """
+login_url = "https://volweb.utk.edu/~iige/"
 
 access_token = None
-	
-def get_URL():
-	"""
-		Temporary Workaround:
-		The access token is available for a very short time after a successful
-		login so if your access token is not available, decrease the sleep time
-		slowly until it prints without crashing webview.
-	"""
-	time.sleep(8)
-	link = webview.get_current_url()
-	print(link)
-	
-	"""
-	If you haven't authorized access to GiftLink with Facebook once before,
-	comment out the following line so you can grant access first.
-	"""
-	webview.destroy_window()
 	
 def main():
 	#Remove all of the spaces/newlines to make a valid URL
 	cthread = threading.current_thread()
 	print(cthread.name)
 	global url
-	url = login_url.replace("\n", "")
-	url = url.replace("\r", "")
-	url = url.replace(" ", "")
-	print(url)
 	newMainThread = threading.Thread(target = newMain)
 	newMainThread.start()
 	print("Active Threads: ", end = "")
@@ -51,11 +22,7 @@ def main():
 	
 
 def newMain():
-	print("I am the new main")
-	global url
-	urlGrabber = threading.Thread(target = get_URL)
-	urlGrabber.start()
-	urlGrabber.join()
+	print("Access Token Should Be In Webview after Login")
 
 if __name__ == "__main__":
 	main()
